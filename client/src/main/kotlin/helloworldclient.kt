f2a17f8705232ab9daa002f00969aeb536b92689
realdude1@gmx.net <welcome1!>
2017-05-09 15:57:50 +0200

    Updated dependencies to newest version, modified implementation accordingly - kotlin 1.1.2 - grpc 1.3.0

--- client/src/main/kotlin/helloworldclient.kt
+++ client/src/main/kotlin/helloworldclient.kt
@@ -8,7 +8,12 @@ import java.util.concurrent.TimeUnit
 fun main(args: Array<String>) {
     val channel = ManagedChannelBuilder.forAddress("localhost", 8080).usePlaintext(true).build()
     val blockingStub = GreeterGrpc.newBlockingStub(channel)
-    val request = HelloRequest.newBuilder().setName("Steve").build()
+//    val request = HelloRequest.newBuilder().setName("Steve").build()
+    val builder = HelloRequest.newBuilder()
+    with(builder) {
+        name = "Steve"
+    }
+    val request = builder.build()
     try {
         println("Calling server")
         val response = blockingStub.sayHello(request)
